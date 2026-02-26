b1c11f2117df7c562d165c4fd486405fdf8d994c
Volodymyr Mykhailov <harvel.bk@gmail.com>
2026-02-26 22:12:21 +0200

    Refactor project structure by cleaning up comments, updating .gitignore, and removing unused files

--- lib/soundsync/application.ex
+++ lib/soundsync/application.ex
@@ -1,6 +1,4 @@
 defmodule Soundsync.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
   @moduledoc false
 
   use Application
@@ -12,20 +10,13 @@ defmodule Soundsync.Application do
       Soundsync.Repo,
       {DNSCluster, query: Application.get_env(:soundsync, :dns_cluster_query) || :ignore},
       {Phoenix.PubSub, name: Soundsync.PubSub},
-      # Start a worker by calling: Soundsync.Worker.start_link(arg)
-      # {Soundsync.Worker, arg},
-      # Start to serve requests, typically the last entry
       SoundsyncWeb.Endpoint
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Soundsync.Supervisor]
     Supervisor.start_link(children, opts)
   end
 
-  # Tell Phoenix to update the endpoint configuration
-  # whenever the application is updated.
   @impl true
   def config_change(changed, _new, removed) do
     SoundsyncWeb.Endpoint.config_change(changed, removed)
--- lib/soundsync_web.ex
+++ lib/soundsync_web.ex
@@ -1,21 +1,5 @@
 defmodule SoundsyncWeb do
-  @moduledoc """
-  The entrypoint for defining your web interface, such
-  as controllers, components, channels, and so on.
-
-  This can be used in your application as:
-
-      use SoundsyncWeb, :controller
-      use SoundsyncWeb, :html
-
-  The definitions below will be executed for every controller,
-  component, etc, so keep them short and clean, focused
-  on imports, uses and aliases.
-
-  Do NOT define functions inside the quoted expressions
-  below. Instead, define additional modules and import
-  those modules here.
-  """
+  @moduledoc false
 
   def static_paths, do: ~w(assets fonts images favicon.ico robots.txt)
 
@@ -23,7 +7,6 @@ defmodule SoundsyncWeb do
     quote do
       use Phoenix.Router, helpers: false
 
-      # Import common connection and controller functions to use in pipelines
       import Plug.Conn
       import Phoenix.Controller
     end
