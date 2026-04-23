e428d8dfafa656aa31848e4dbdc11e2c29f4dc52
Volodymyr Mykhailov <harvel.bk@gmail.com>
2026-04-23 18:39:37 +0300

    feat: add ok dependency version 2.3 to mix.exs and mix.lock

--- mix.exs
+++ mix.exs
@@ -54,7 +54,8 @@ defmodule Soundsync.MixProject do
       {:gettext, "~> 1.0"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.2.0"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:ok, "~> 2.3"}
     ]
   end
 
