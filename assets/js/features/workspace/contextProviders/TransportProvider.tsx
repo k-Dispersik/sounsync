030327c4617b35c661bd7feef1ca3853c5455967
Volodymyr Mykhailov <harvel.bk@gmail.com>
2026-06-09 21:14:16 +0300

    refactor: add player logic in context

--- assets/js/features/workspace/contextProviders/TransportProvider.tsx
+++ assets/js/features/workspace/contextProviders/TransportProvider.tsx
@@ -1,5 +1,8 @@
-import { createContext, useContext } from "react";
-import { useTransport } from "../hooks/useTransport";
+import { createContext, useContext, useMemo, useState } from "react";
+import { Player } from "../services/audio/Player";
+import { Scheduler } from "../services/audio/Sheduler";
+import { AudioEngine } from "../services/audio/AudioEngine";
+import { SampleBank } from "../services/audio/SampleBank";
 
 type TransportContextType = {
     isPlaying: boolean;
@@ -13,10 +16,50 @@ type TransportContextType = {
 const TransportContext = createContext<TransportContextType | null>(null);
 
 export default function TransportProvider({ children }: { children: React.ReactNode }) {
-    const values = useTransport();
+    const scheduler = useMemo(() => new Scheduler(), []);
+
+    const engine = useMemo(() => {
+        const ctx = new AudioContext();
+        return new AudioEngine(ctx, new SampleBank(ctx));
+    }, []);
+
+    const player = useMemo(() => {
+        return new Player(scheduler, engine);
+    }, [scheduler, engine]);
+
+    const [isPlaying, setIsPlaying] = useState(false);
+    const [playheadPosition, setPlayheadPosition] = useState(0);
+
+    const transport = useMemo(() => {
+        return {
+            isPlaying,
+            playheadPosition,
+
+            play: () => {
+                setIsPlaying(true);
+                player.start();
+            },
+
+            pause: () => {
+                setIsPlaying(false);
+                player.pause();
+            },
+
+            stop: () => {
+                setIsPlaying(false);
+                setPlayheadPosition(0);
+                player.stop();
+            },
+
+            setPosition: (ms: number) => {
+                setPlayheadPosition(ms);
+                player.seek(ms);
+            },
+        };
+    }, [player, isPlaying, playheadPosition]);
 
     return (
-        <TransportContext.Provider value={values}>
+        <TransportContext.Provider value={transport}>
             {children}
         </TransportContext.Provider>
     );
@@ -25,7 +68,7 @@ export default function TransportProvider({ children }: { children: React.ReactN
 export function useTransportContext() {
     const ctx = useContext(TransportContext);
     if (!ctx) {
-        throw new Error("useTransport must be used within a TransportProvider");
+        throw new Error("useTransport must be used within TransportProvider");
     }
-    return ctx as TransportContextType;
+    return ctx;
 }
